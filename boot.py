156903a50501c51c66119e5abbf1eaa5097282c4
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-05-26 10:13:32 -0500

    Set USB MIDI names

--- boot.py
+++ boot.py
@@ -4,8 +4,9 @@
 
 import os
 import storage
-import usb_hid
 import usb_cdc
+import usb_hid
+import usb_midi
 
 # Rename drive
 storage.remount("/", readonly=False)
@@ -16,6 +17,14 @@ mnt.label = "SYNTHIOTA"
 storage.remount("/", readonly=False, disable_concurrent_write_protection=True)
 usb_hid.disable()
 usb_cdc.enable(console=True, data=False)
+
+# Rename MIDI interface
+usb_midi.set_names(
+    streaming_interface_name="synthiota Audio",
+    audio_control_interface_name="synthiota MIDI",
+    in_jack_name="synthiota Drum Machine",
+    out_jack_name="synthiota Drum Machine",
+)
 
 # Create samples directory if not found
 try:
